e93c4bcd8ddf34d5f022d83e295765957a1ae049
Bivash Thakuri <tbivash3@gmail.com>
2021-05-05 00:18:44 -0500

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'MazeGeneratorAndSolver';
+
+  width = 31;
+
+  height = 20;
+
+  length = Array.from(Array(this.width * this.height).keys());
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    console.log(document.getElementById('box100'));
+    document.getElementById('box100')?.classList.add('right-border-collapse');
+    document.getElementById('box100')?.classList.add('left-border-collapse');
+    document.getElementById('box100')?.classList.add('top-border-collapse');
+    document.getElementById('box100')?.classList.add('button-border-collapse');
+  }
 }
---
+++ src/app/entity/Box.ts
@@ -0,0 +1,4 @@
+interface Box {
+  id: number;
+  adjacents: Box[];
+}
